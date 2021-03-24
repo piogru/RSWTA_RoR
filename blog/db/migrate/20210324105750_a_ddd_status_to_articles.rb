40b37c3d6d52873ec55a8801775fc9f98e270af2
piogru <ssmm19a@gmail.com>
2021-03-24 12:34:15 +0000

    123

---
+++ blog/db/migrate/20210324105750_a_ddd_status_to_articles.rb
@@ -0,0 +1,4 @@
+class ADddStatusToArticles < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
---
+++ blog/db/migrate/20210324105803_a_ddd_status_to_comments.rb
@@ -0,0 +1,4 @@
+class ADddStatusToComments < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
